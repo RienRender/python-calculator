4d17c1206d4000cff96adb17d0f9eeae10a369ff
RienRender <67402796+RienRender@users.noreply.github.com>
2024-11-13 08:21:56 +0000

    Added the testcase

--- test_calculator.py
+++ test_calculator.py
@@ -8,8 +8,27 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(2, 2), 4)
+
+    def test_sub(self):
+        self.assertEqual(self.calc.subtract(1, 2), -1)
+        self.assertEqual(self.calc.subtract(2, 2), 0)
+
+
+    def test_multi(self):
+        self.assertEqual(self.calc.multiply(1, 2), 2)
+        self.assertEqual(self.calc.multiply(3, 2), 6)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(4, 2), 2)
+        self.assertEqual(self.calc.divide(6, 2), 3)
+
+    def test_modu(self):
+        self.assertEqual(self.calc.modulo(5, 2), 1)
+        self.assertEqual(self.calc.modulo(3, 2), 1)
 
     # Add the following test methods to the TestCalculator class:
 
 if __name__ == '__main__':
     unittest.main()
+
